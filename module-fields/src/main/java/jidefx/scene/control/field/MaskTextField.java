ab55b4aea88a5e5d979d7040d2a2384309ed5f7b
jidesoft <support@jidesoft.com>
2014-05-04 16:40:17 -0700

    fixed issue #12 where setInputMask to null caused NPE

--- module-fields/src/main/java/jidefx/scene/control/field/MaskTextField.java
+++ module-fields/src/main/java/jidefx/scene/control/field/MaskTextField.java
@@ -41,24 +41,17 @@ import java.util.Map;
  * <b >InputMask</b>
  * <p>
  * MaskTextField has setInputMask(String mask) to set the mask we mentioned above. The pre-defined mask characters are:
- * <ul>
- * <li>A	INPUT_MASK_LETTER	ASCII alphabetic character required. A-Z, a-z.
- * <li>N	INPUT_MASK_DIGIT_OR_LETTER	ASCII alphanumeric character required. A-Z, a-z, 0-9.</li>
- * <li>X	INPUT_MASK_ANY_NON_SPACE	Any character required except spaces.
- * <li>H	INPUT_MASK_HAX	Hexadecimal character required. A-F, a-f, 0-9.
- * <li>D	INPUT_MASK_DIGIT_NON_ZERO	ASCII digit required. 1-9.
- * <li>9	INPUT_MASK_DIGIT	ASCII digit required. 0-9.
- * <li>8	INPUT_MASK_DIGIT_0_TO_8	ASCII digit required. 0-8.
- * <li>7, 6, 5, 4, 3, 2 and so on which means only allows from 0 to that number
- * <li>2	INPUT_MASK_DIGIT_0_TO_2	ASCII digit required. 0-2.
- * <li>1	INPUT_MASK_DIGIT_0_TO_1	ASCII digit required. 0-1, for example, a binary number
- * <li>0	INPUT_MASK_DIGIT_ZERO	0 required
- * <li>G	INPUT_MASK_DIGIT_GROUP	Indicates a Group. This is the special one, we will discuss it later
- * </ul>
- * If the setInitialText is not called, the InputMask will be used to create the initial text for the field. We create
- * it by removing all masks, replace them with spaces or the specified placeholder character, and leave the non-mask
- * characters where they are. The placeholder character can be set using setPlaceholderCharacter(char). See below. In
- * both cases, the InputMask is 999-99-9999.
+ * <ul> <li>A	INPUT_MASK_LETTER	ASCII alphabetic character required. A-Z, a-z. <li>N	INPUT_MASK_DIGIT_OR_LETTER	ASCII
+ * alphanumeric character required. A-Z, a-z, 0-9.</li> <li>X	INPUT_MASK_ANY_NON_SPACE	Any character required except
+ * spaces. <li>H	INPUT_MASK_HAX	Hexadecimal character required. A-F, a-f, 0-9. <li>D	INPUT_MASK_DIGIT_NON_ZERO	ASCII
+ * digit required. 1-9. <li>9	INPUT_MASK_DIGIT	ASCII digit required. 0-9. <li>8	INPUT_MASK_DIGIT_0_TO_8	ASCII digit
+ * required. 0-8. <li>7, 6, 5, 4, 3, 2 and so on which means only allows from 0 to that number
+ * <li>2	INPUT_MASK_DIGIT_0_TO_2	ASCII digit required. 0-2. <li>1	INPUT_MASK_DIGIT_0_TO_1	ASCII digit required. 0-1, for
+ * example, a binary number <li>0	INPUT_MASK_DIGIT_ZERO	0 required <li>G	INPUT_MASK_DIGIT_GROUP	Indicates a Group. This
+ * is the special one, we will discuss it later </ul> If the setInitialText is not called, the InputMask will be used to
+ * create the initial text for the field. We create it by removing all masks, replace them with spaces or the specified
+ * placeholder character, and leave the non-mask characters where they are. The placeholder character can be set using
+ * setPlaceholderCharacter(char). See below. In both cases, the InputMask is 999-99-9999.
  * <p>
  * <b>ConversionMask</b>
  * <p>
@@ -68,13 +61,10 @@ import java.util.Map;
  * automatically convert the entered character to another character. This mask is optional. It can be set using
  * setConversionMask(String mask). If not set, there will be no conversion. If you ever set the mask, please make sure
  * they have the exact same length as the InputMask, and have a valid conversion mask character at the exact position
- * where there is an InputMask character.
- * <ul>
- * <li>U	CONVERSION_MASK_UPPER_CASE	Uppercase required. If user enters a lowercase letter, it will be automatically
- * converted to the corresponding uppercase letter.
+ * where there is an InputMask character. <ul> <li>U	CONVERSION_MASK_UPPER_CASE	Uppercase required. If user enters a
+ * lowercase letter, it will be automatically converted to the corresponding uppercase letter.
  * <li>L	CONVERSION_MASK_LOWER_CASE	Lowercase required. If user enters an uppercase letter, it will be automatically
- * converted to the corresponding lowercase letter.
- * <li>Any other undefined chars	CONVERSION_MASK_IGNORE	No conversion
+ * converted to the corresponding lowercase letter. <li>Any other undefined chars	CONVERSION_MASK_IGNORE	No conversion
  * </ul>
  * <p>
  * {@code RequiredMask}
@@ -82,12 +72,10 @@ import java.util.Map;
  * The RequiredMask is to indicate whether the character on a particular position is required. It is again optional. It
  * can be set using setRequiredMask(String mask). If not set, a valid non-space character is required on all the
  * positions. If you ever set the mask, please make sure they have the same length as the InputMask, and have a valid
- * required mask character at the exact position where there is an InputMask character.
- * <ul>
+ * required mask character at the exact position where there is an InputMask character. <ul>
  * <li>R	REQUIRED_MASK_REQUIRED	Required. Users must enter a valid character that matches with the mask on this
- * position.
- * <li>Any other undefined chars	REQUIRED_MASK_NOT_REQUIRED	Not required. User can enter a space at this position.
- * </ul>
+ * position. <li>Any other undefined chars	REQUIRED_MASK_NOT_REQUIRED	Not required. User can enter a space at this
+ * position. </ul>
  */
 @SuppressWarnings({"Convert2Lambda", "SpellCheckingInspection"})
 public class MaskTextField extends TextField implements DecorationSupport {
@@ -334,11 +322,12 @@ public class MaskTextField extends TextField implements DecorationSupport {
 
     private String getInitialTextFromMask() {
         String mask = getInputMask();
-        Map<Character, Callback<Character, Boolean>> maskVerifiers = getInputMaskVerifiers();
-        for (Character c : maskVerifiers.keySet()) {
-            mask = mask.replace(c, getPlaceholderCharacter());
+        if (mask != null) {
+            Map<Character, Callback<Character, Boolean>> maskVerifiers = getInputMaskVerifiers();
+            for (Character c : maskVerifiers.keySet()) {
+                mask = mask.replace(c, getPlaceholderCharacter());
+            }
         }
-        _fixedText = mask;
         return mask;
     }
 
@@ -836,7 +825,7 @@ public class MaskTextField extends TextField implements DecorationSupport {
      * month/day/year). The InputMask for it is "19/39/9999". The ConversionMask is "_x/_y/____" and we defined
      * conversion callbacks for both 'x' and 'y' to make sure the month is between 1 and 12, the day of the month is
      * between 1 and 31.
-     * <p/>
+     * <p>
      * The initial text was set to the today's date.
      *
      * @return a MaskTextField for Date.
@@ -876,7 +865,7 @@ public class MaskTextField extends TextField implements DecorationSupport {
      * hour/minute/second AM/PM). The InputMask for it is "19:59:59 xm". The InputMask 'x' and 'm' were defined to make
      * sure the entered letter is 'A' or 'P', and 'M' respectively. The ConversionMask is "_x:__:__ UU" and we defined
      * conversion callbacks for the conversion mask 'x' to make sure the hour is between 1 and 12.
-     * <p/>
+     * <p>
      * The initial text was set to the current time.
      *
      * @return a MaskTextField for Date.
@@ -922,7 +911,7 @@ public class MaskTextField extends TextField implements DecorationSupport {
      * A static method which will create a MaskTextField for the Time in the 24-hour format (in the order of
      * hour/minute/second). The InputMask for it is "29/59/59". The ConversionMask is "_x/__/__" and we defined
      * conversion callbacks for the conversion mask named 'x' to make sure the hour is between 0 and 23.
-     * <p/>
+     * <p>
      * The initial text was set to the current time.
      *
      * @return a MaskTextField for Date.

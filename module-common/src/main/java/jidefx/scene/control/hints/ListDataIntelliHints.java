007590fab6a2cd4c4ed1a5e6c2e67e8663f679a1
aalmiray <aalmiray@gmail.com>
2015-04-09 13:31:34 +0200

    Update license headers on all source files

--- module-common/src/main/java/jidefx/scene/control/hints/ListDataIntelliHints.java
+++ module-common/src/main/java/jidefx/scene/control/hints/ListDataIntelliHints.java
@@ -1,7 +1,22 @@
 /*
- * @(#)ListDataIntelliHints.java 5/19/2013
+ * Copyright (c) 2002-2015, JIDE Software Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright 2002 - 2013 JIDE Software Inc. All rights reserved.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package jidefx.scene.control.hints;
 
